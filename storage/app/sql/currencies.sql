9b9dfb28fe5dc62de2e87f5be46cf94c25036f4d
Dismus Ng'eno <dismuskiplimo@gmail.com>
2022-06-13 14:51:06 +0300

    Added missing sql files

---
+++ storage/app/sql/currencies.sql
@@ -0,0 +1,4 @@
+INSERT INTO currencies (country, currency, code, symbol) VALUES
+    ('Kenya', 'Kenyan Shilling','KES','KES'),
+    ('United Kingdom', 'British Pound','GBP','£'),
+    ('United States of America', 'US Dollar','USD','$');
